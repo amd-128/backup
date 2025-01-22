9ded7cf3ae35ab7b0a16fe110aacb324403a6a2b
amd-128 <x86_128@outlook.com>
2025-01-22 16:24:53 +0800

    添加压缩选项

--- backup.sh
+++ backup.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-SOURCE=0
-TARGET=0
+src=
+tgt=
 source_array=()
 target_dir_array=()
+zip_program=
+if [ $1 == "J" ]
+then
+	zip_program=".xz"
+elif [ $1 == "j" ]
+then
+	zip_program=".bz2"
+elif [ $1 == "z" ]
+then
+	zip_program=".gz"
+fi
 for file in $(cat backupfile)
 do
 	if [ $file == "source:" ]
 	then
-		SOURCE=1
-		TARGET=0
+		src=1
+		tgt=0
 	elif [ $file == "target:" ]
 	then
-		TARGET=1
-		SOURCE=0
+		tgt=1
+		src=0
 	else
-		if [ $SOURCE == 1 ]
+		if [ $src == 1 ]
 		then
 			source_array[${#source_array[@]}]=$file
-		elif [ $TARGET == 1 ]
+		elif [ $tgt == 1 ]
 		then
 			target_dir_array[${#target_dir_array[@]}]=$file
 		fi
@@ -29,7 +40,7 @@ for target_dir in ${target_dir_array[@]}
 do
 	NOWTIME=`date '+%Y%m%d_%H%M%S'`
 	mkdir -pv $target_dir
-	tar -cvf $target_dir/backup_$NOWTIME.tar ${source_array[@]}
+	tar -c$1vf $target_dir/backup_$NOWTIME.tar$zip_program ${source_array[@]}
 done
 
 echo "主人~请稍等~正在同步磁盘~"
